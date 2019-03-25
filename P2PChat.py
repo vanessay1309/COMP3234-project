b299e6c63d1e97c73b3d41da1aa3337a6301e38a
vanessay1309 <u3530531@hku.hk>
2019-03-25 17:26:52 +0800

    Initial setup

---
+++ P2PChat.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+# Student name and No.:
+# Student name and No.: 3035305318 Yam Mei Ki
+# Development platform:
+# Python version:
+# Version:
+
+import socket
+import sys
+
+def main(argv):
+	# set port number
+	# default is 32341 if no input argument
+    print ("testing")
+
+
+if __name__ == '__main__':
+	if len(sys.argv) != 4:
+		print("Usage: FTclient.py <Server_addr> <Server_port> <filename>")
+		sys.exit(1)
+	main(sys.argv)
